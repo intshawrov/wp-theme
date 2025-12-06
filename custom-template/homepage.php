a5f9bffd55fc5aa81a2b9a38997c9354ae4946ce
Shawrov <intshawrov@gmail.com>
2025-12-06 18:10:54 +0600

    updated

--- custom-template/homepage.php
+++ custom-template/homepage.php
@@ -66,8 +66,7 @@ $slider_section = get_field('slider_section');
 
                     <?php  
                         $about_section = get_field('about_section');
-                        // echo"<pre>";
-                        // print_r($about_section);
+
                     ?>
     <section class="about-section" style="background-image: url(<?php echo $about_section['background_image']['url'] ?>);">
         <div class="auto-container">
@@ -127,14 +126,18 @@ $slider_section = get_field('slider_section');
     <!-- Services Section -->
 
                 <?php 
-                $specialazation_section = get_field('specialazation_section');
+                $specialization_section = get_field('specialization_section');
+              $service_slider = $specialization_section['service_slider'] ?? [];
+
                 ?>
     <section class="services-section">
         <div class="upper-box" style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/background/2.jpg);">
             <div class="auto-container">    
                 <div class="sec-title text-center light">
                     <span class="float-text">Specialization</span>
-                    <h2><?php echo $specialazation_section['section_title']; ?></h2>
+                    <?php if( !empty($specialization_section['section_title']) ): ?>
+                    <h2><?php echo esc_html($specialization_section['section_title']); ?></h2>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -143,24 +146,57 @@ $slider_section = get_field('slider_section');
             <div class="auto-container">
                 <div class="services-carousel owl-carousel owl-theme">
                     <!-- Service Block -->
+                     <?php if( !empty($service_slider) && is_array($service_slider) ): ?>
+                    <?php foreach( $service_slider as $slider ): 
+                        $learn_more_button = $slider['learon_more_button'] ?? null;
+                    ?> 
+
                     <div class="service-block">
                         <div class="inner-box">
                             <div class="image-box">
-                                <figure class="image"><a href="service-detail.html"><img src="<?php echo get_template_directory_uri(); ?>/images/resource/service-1.jpg" alt=""></a></figure>
+                                <?php if(!empty($slider['image']['url'])): ?>
+                                    <figure class="image">
+                                        <a href="<?php echo esc_url($learn_more_button['url'] ?? '#'); ?>">
+                                            <img src="<?php echo esc_url($slider['image']['url']); ?>" alt="">
+                                        </a>
+                                    </figure>
+                                <?php endif; ?>
                             </div>
+
                             <div class="lower-content">
-                                <h3><a href="service-detail.html">Architectural Design</a></h3>
-                                <div class="text">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they.</div>
-                                <div class="link-box">
-                                    <a href="service-detail.html">Lorn More <i class="fa fa-long-arrow-right"></i></a>
+                                <h3>
+                                    <a href="<?php echo esc_url($learn_more_button['url'] ?? '#'); ?>">
+                                        <?php echo esc_html($slider['title'] ?? ''); ?>
+                                    </a>
+                                </h3>
+
+                                <div class="text">
+                                    <?php echo esc_html($slider['description'] ?? ''); ?>
                                 </div>
+
+                                <?php if($learn_more_button): 
+                                    $link_url    = $learn_more_button['url'] ?? '#';
+                                    $link_title  = $learn_more_button['title'] ?? 'Learn More';
+                                    $link_target = !empty($learn_more_button['target']) ? $learn_more_button['target'] : '_self';
+                                ?>
+                                    <div class="link-box">
+                                        <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                                            <?php echo esc_html($link_title); ?> <i class="fa fa-long-arrow-right"></i>
+                                        </a>
+                                    </div>
+                                <?php endif; ?>
+
                             </div>
                         </div>
                     </div>
-                </div>
+
+                    <?php endforeach; ?>
+                <?php endif; ?>
+
             </div>
         </div>
-    </section>
+    </div>
+</section>
     <!--End Services Section -->
 
     <!-- Fun Fact Section -->
